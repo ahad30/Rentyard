c0200a9a2b5153729d6641ee328f956293a99036
ahad30 <mohiminulislamahad@gmail.com>
2025-06-30 01:39:04 +0600

    sadd

--- src/components/Forms/AminitiesForm.jsx
+++ src/components/Forms/AminitiesForm.jsx
@@ -51,7 +51,7 @@ const AmenitiesForm = ({ data = {}, onChange }) => {
           placeholder="Search amenities"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-full focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
         />
       </div>
 
